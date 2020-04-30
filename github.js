5d42140cbc63901cf54cea694f4a7958d2a6c295
vd89 <me@daxcodes.in>
2020-04-30 14:10:05 +0530

    the github search with the axios

--- github.js
+++ github.js
@@ -1,28 +1,32 @@
 class GitHub {
-    constructor() {
-        this.client_id = 'c5868f39180303a9e8e4';
-        this.client_secret = '32735d5e618c1fc35ce25bcc7c44b0472f590a7c';
-        this.repos_count = 5;
-        this.repos_sort = 'created : asc';
-    }
-    async getUser(user) {
-        const profileResponse = await fetch(
-            `https://api.github.com/users/${user}?client_id=${
-            this.client_id
-            }&client_secret=${this.client_secret}`,
-        );
-        const repoResponse = await fetch(
-            `https://api.github.com/users/${user}/repos?per_page=${
-            this.repos_count
-            }&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${
-            this.client_secret
-            }`
-        );
-        const profile = await profileResponse.json()
-        const repos = await repoResponse.json()
-        return {
-            profile,
-            repos
-        }
-    }
+  constructor() {
+    this.repos_count = 6;
+    this.repos_sort = "created : asc";
+  }
+  async getUser(user) {
+    const profileResponse = await axios
+      .get(`https://api.github.com/users/${user}`)
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => {
+        throw err;
+      });
+    const repoResponse = await axios
+      .get(
+        `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`
+      )
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => {
+        throw err;
+      });
+    const profile = profileResponse;
+    const repos = repoResponse;
+    return {
+      profile,
+      repos,
+    };
+  }
 }
--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 const github = new GitHub();
 const ui = new UI();
 
-const searchUser = document.getElementById('searchUser');
+const searchUser = document.getElementById("searchUser");
 
-searchUser.addEventListener('keyup', e => {
-    const userText = e.target.value;
-    if (userText !== '') {
-        // console.log(userText);
-        github.getUser(userText).then(data => {
-            if (data.profile.message === 'Not Found') {
-                // show alert
-                // console.log('user not found', 'alert alert-danger');
-                ui.showAlert('User Not Found', 'alert alert-danger');
-            } else {
-                console.log(data.profile);
-                // console.log(data.repos);
-                ui.showProfile(data.profile);
-                ui.showRepos(data.repos);
-            }
-        });
-    } else {
-        // clear profile
-        ui.clearProfile();
-    }
+searchUser.addEventListener("keyup", (e) => {
+  const userText = e.target.value;
+  if (userText !== "") {
+    github.getUser(userText).then((data) => {
+      if (data.profile.message === "Not Found") {
+        ui.showAlert("User Not Found", "alert alert-danger");
+      } else {
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    });
+  } else {
+    ui.clearProfile();
+  }
 });
