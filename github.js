707b327084b42b461694e8d1831748c1d51e3928
vd89 <me@daxcodes.in>
2020-05-01 17:06:11 +0530

    added the client id and client secret keys

--- github.js
+++ github.js
@@ -1,14 +1,14 @@
 class GitHub {
   constructor() {
-    this.client_id = "c5868f39180303a9e8e4";
-    this.client_secret = "32735d5e618c1fc35ce25bcc7c44b0472f590a7c";
+    this.client_id = 'c5868f39180303a9e8e4';
+    this.client_secret = '32735d5e618c1fc35ce25bcc7c44b0472f590a7c';
     this.repos_count = 6;
-    this.repos_sort = "created : asc";
+    this.repos_sort = 'created : asc';
   }
   async getUser(user) {
     const profileResponse = await axios
       .get(
-        `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+        `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`,
       )
       .then((res) => {
         return res.data;
@@ -18,7 +18,7 @@ class GitHub {
       });
     const repoResponse = await axios
       .get(
-        `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+        `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`,
       )
       .then((res) => {
         return res.data;
